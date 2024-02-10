2fa956337a91a65d91c07152cb047413d0cf212e
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2024-02-10 11:08:45 +0530

    project initialize

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="bg-red-300">
+      Diwan Sachidu
+      <Button variant="ghost">click here</Button>
+    </div>
+  );
+}
