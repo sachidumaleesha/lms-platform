d76f8e09fec84bdd24e247dc24b1a56a7ec2c65c
sachidumaleesha <diwansachidu2000@gmail.com>
2024-03-01 19:26:04 +0530

    add some modification

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
+
 const AuthLayout = ({
   children
 }: {
   children: React.ReactNode
 }) => {
   return ( 
-    <div className="h-full flex items-center justify-center">
-      {children}
+    <div className="h-full lg:grid lg:grid-cols-12 flex justify-center items-center">
+      <div className="lg:col-span-6 hidden lg:inline">
+        <Image src="/auth-image-coding.jpg" width={500} height={500} alt="auth-image" className="object-cover w-full h-screen"/>
+      </div>
+      <div className="lg:col-span-6 flex justify-center items-center">
+        {children}
+      </div>
     </div>
    );
 }
