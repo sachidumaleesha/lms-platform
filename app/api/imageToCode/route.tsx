07dbd17c37685f054b9b51f8b2bffc9212ee1a46
sachidumaleesha <diwansachidu2000@gmail.com>
2024-03-30 00:10:06 +0530

    update image to code model to claude

--- app/api/imageToCode/route.tsx
+++ app/api/imageToCode/route.tsx
@@ -1,82 +1,54 @@
 import { NextResponse } from "next/server";
-
-const {
-  GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
-} = require("@google/generative-ai");
-
 import axios from "axios";
 import { auth } from "@clerk/nextjs";
+import Anthropic from '@anthropic-ai/sdk';
+
+const anthropic = new Anthropic({
+  apiKey: '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',
+});
 
 export async function POST(req: Request) {
-
-  const {userId} = auth();
-
-  if(!userId){
-    return new NextResponse("Unauthorized", {status: 401})
+  const { userId } = auth();
+  if (!userId) {
+    return new Response("Unauthorized", { status: 401 });
   }
 
   const { imageUrl } = await req.json();
-  const MODEL_NAME = "gemini-1.0-pro-vision-latest";
-  const API_KEY = process.env.GEMINI_API;
 
   try {
-    const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+    const imageResponse = await fetch(imageUrl);
+    const imageBuffer = await imageResponse.arrayBuffer();
+    const imageBase64 = Buffer.from(imageBuffer).toString('base64');
 
-    const generationConfig = {
-      temperature: 0.4,
-      topK: 32,
-      topP: 1,
-      maxOutputTokens: 4096,
-    };
-
-    const safetySettings = [
-      {
-        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-      },
-    ];
-
-    const imageResponse = await axios.get(imageUrl, {
-      responseType: "arraybuffer",
-    });
-    const imageBase64 = Buffer.from(imageResponse.data).toString("base64");
-
-    const parts = [
-      {
-        inlineData: {
-          mimeType: "image/png",
-          data: imageBase64,
-        },
-      },
-      {
-        text: "\n\nGive me the code of the above image with proper indentation. \n\n",
-      },
-    ];
-
-    const result = await model.generateContent({
-      contents: [{ role: "user", parts }],
-      generationConfig,
-      safetySettings,
+    const msg = await anthropic.messages.create({
+      model: "claude-3-opus-20240229",
+      max_tokens: 1000,
+      temperature: 0,
+      system: "Give me the code of the above image with proper indentation.",
+      messages: [
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "image",
+              "source": {
+                "type": "base64",
+                "media_type": "image/jpeg",
+                "data": imageBase64
+              }
+            }
+          ]
+        }
+      ]
     });
 
-    return NextResponse.json(result);
+    return new Response(JSON.stringify(msg), {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
   } catch (error) {
     console.log("[ImageToCode]", error);
-    return new NextResponse("Internal Error", { status: 500 });
+    return new Response("Internal Error", { status: 500 });
   }
 }
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -6,13 +6,13 @@ async function main() {
   try {
     await database.category.createMany({
       data: [
-        { name: "Computer Science" },
-        { name: "Music" },
-        { name: "Fitness" },
-        { name: "Photography" },
-        { name: "Accounting" },
-        { name: "Engineering" },
-        { name: "Filming" },
+        { name: "Web Development" },
+        { name: "AI/ML" },
+        { name: "Python" },
+        { name: "React JS" },
+        { name: "Angular" },
+        { name: "Java" },
+        { name: "Docker" },
       ]
     });
 
